d27c137ca7504eb9c773a321ddc41ab270537807
taxijjang <gw9122@naver.com>
2019-01-30 23:08:58 +0900

    dp

---
+++ 11055.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int arr[1010];
+int dp[1010];
+
+int max(int a, int b) {
+	return a > b ? a : b;
+}
+int main(void) {
+	int N;
+	scanf("%d", &N);
+
+	for (int i = 1; i <= N; i++) {
+		scanf("%d", &arr[i]);
+	}
+	
+	for (int i = 0; i <= N; i++) {
+		dp[i] = 0;
+	}
+	int Max = 0;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j < i; j++) {
+			if (arr[i] > arr[j]) {
+				dp[i] = max(dp[j], dp[i]);
+			}
+		}
+		dp[i] += arr[i];
+		if (Max < dp[i]) Max = dp[i];
+	}
+	printf("%d", Max);
+
+	return 0;
+}

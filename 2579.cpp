7e74a84130f6c52daaf4c58d6231ff125baf6fde
taxijjang <gw9122@naver.com>
2019-01-30 23:22:56 +0900

    dp

--- 2579.cpp
+++ 2579.cpp
@@ -4,24 +4,23 @@ int arr[301];
 int dp[301];
 
 int max(int a, int b) {
-	if (a > b) return a;
-	return b;
+	return a > b ? a : b;
 }
 int main(void) {
 	int N;
 	scanf("%d", &N);
 
-	int a;
-	for (int n = 1, a; n <= N; n++) {
-		scanf("%d", &a);
-		arr[n] = a;
+	for (int i = 1; i <= N; i++) {
+		scanf("%d", &arr[i]);
 	}
-	dp[0] = 0;
-	dp[1] = max(arr[0] + arr[1], arr[1]);
-	dp[2] = max(arr[0] + arr[2], arr[1] + arr[2]);
 
-	for (int d = 3; d <= N; d++) {
-		dp[d] += max(dp[d - 2] + arr[d], dp[d - 3] + arr[d - 1] + arr[d]);
+	dp[0] = 0;
+	dp[1] = arr[1];
+	dp[2] = arr[1] + arr[2];
+
+	for (int i = 3; i <= N; i++) {
+		dp[i] = max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]);
 	}
+
 	printf("%d", dp[N]);
 }

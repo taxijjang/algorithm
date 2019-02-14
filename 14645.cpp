9755667a6f94a2876287774d30cbc8dbe7d05c88
taxijjang <gw9122@naver.com>
2019-02-15 01:34:37 +0900

    output

---
+++ 14645.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	int N, K;
+	scanf("%d %d", &N, &K);
+
+	for (int i = 0; i < N; i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+	}
+
+	printf("%s", "ºñ¿ÍÀÌ");
+
+	return 0;
+}

3377985fb0e370f23510177812278943d494c002
taxijjang <gw9122@Naver.com>
2019-03-14 20:21:43 +0900

    bs

--- 2805.cpp
+++ 2805.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
+typedef long long int ll;
 
 using namespace std;
 
-int arr[1000001];
+ll tree[1000001];
 
 int main(void) {
 	int N, M;
 	scanf("%d %d", &N, &M);
 
-	
+	ll max = 1;
 	for (int i = 0; i < N; i++) {
-		scanf("%d", &arr[i]);
+		scanf("%lld", &tree[i]);
+		if (max < tree[i]) max = tree[i];
 	}
 
-	int max = 1000000007;
-	int nin = 0;
-	int sum;
-	while (nin + 1 <max) {
-		sum = 0;
-		int mid = (max + nin) / 2;
+	ll start = 1, end = max, mid, cut = 0;
+
+
+	while (start <= end) {
+		ll sum = 0;
+		mid = (start + end) / 2;
 
 		for (int i = 0; i < N; i++) {
-			if (arr[i] > mid) sum += arr[i] - mid;
+			if ((tree[i] - mid) >= 0) {
+				sum += tree[i] - mid;
+			}
 		}
 
-		if (sum >= M) nin = mid;
-		else max = mid;
+		if (sum >= M) {
+			if (cut < mid) cut = mid;
+			start = mid + 1;
+		}
+		else {
+			end = mid - 1;
+		}
 	}
 
-	printf("%d", nin);
+	printf("%d", cut);
+
+
 }

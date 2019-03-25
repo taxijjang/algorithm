ff963013df2cb109be17951733ae608e5a249c5f
taxijjang <gw9122@Naver.com>
2019-03-25 19:58:24 +0900

    dp

--- 10942.cpp
+++ 10942.cpp
@@ -6,18 +6,18 @@ int arr[2001];
 int dp[2001][2001];
 
 int go(int i, int j) {
-	if (dp[i][j] >= 0) 
+	if (dp[i][j] >= 0)
 		return dp[i][j];
 	if (i >= j)
-		return 1;
+		return dp[i][j] = 1;
 	if (i + 1 == j) {
 		if (arr[i] == arr[j])
-			return 1;
+			return dp[i][j] = 1;
 		else
-			return 0;
+			return dp[i][j] = 0;
 	}
 	if (arr[i] == arr[j]) {
-		dp[i][j] = go(i - 1,j - 1);
+		dp[i][j] = go(i + 1, j - 1);
 	}
 	else {
 		return dp[i][j] = 0;
@@ -26,6 +26,8 @@ int go(int i, int j) {
 int main(void) {
 	int N;
 	scanf("%d", &N);
+
+
 	fill(&dp[0][0], &dp[2001][2001], -1);
 
 
@@ -33,9 +35,19 @@ int main(void) {
 		scanf("%d", &arr[i]);
 	}
 
-	int a, b;
-	scanf("%d %d", &a, &b);
+	for (int i = 1; i <= N; i++) {
+		for (int j = i; j <= N; j++) {
+			dp[i][j] = go(i, j);
+		}
+	}
 
-	int z = go(a, b);
+	int M;
 
+	scanf("%d", &M);
+	for (int i = 0; i < M; i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		printf("%d", dp[a][b]);
+	}
+	getchar();
 }

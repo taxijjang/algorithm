23c1140d119214b9f02e05ccdd2d401957d739b2
taxijjang <gw9122@naver.com>
2019-09-10 00:30:38 +0900

    z - 4

--- z - 4.cpp
+++ z - 4.cpp
@@ -3,77 +3,42 @@
 
 using namespace std;
 
-int window(int size, int ii, int jj, vector<vector<int>> &A) {
-	int res = 0;
-	int check = 0;
-	//가로
-	for (int i = ii; i < ii + size; i++) {
-		check = 0;
-		for (int j = jj; j < jj + size; j++) {
-			check += A[i][j];
-			if (i == ii)
-				res = check;
-		}
-		if (res != check)
-			return 0;
-	}
-
-	//세로
-	for (int i = jj; i < jj + size; i++) {
-		check = 0;
-		for (int j = ii; j < ii + size; j++) {
-			check += A[j][i];
-		}
-		if (res != check)
-			return 1;
-	}
-	//오른쪽으로 대각선
-	check = 0;
-	int i = ii, j = jj;
-	for (int s = 0; s < size; s++) {
-		check += A[i++][j++];
-	}
-
-	if (res != check)
-		return 1;
-
-	//왼쪽으로 대각선
-	check = 0;
-	i = ii, j = jj + size - 1;
-	for (int s = 0; s < size; s++) {
-		check += A[i++][j--];
-	}
-	if (res != check)
-		return 1;
-
-	return size;
+bool check(int a, int b, vector<int> &A) {
+	return A[a] < A[b];
 }
-int solution(vector<vector<int>> &A) {
-	// write your code in C++14 (g++ 6.2.0)
+int solution(vector<int> &A) {
 	int res = 1;
-	int m_size = A.size() < A[0].size() ? A.size() : A[0].size();
+	int left = 0, right = 0;
+	//left > right false 
+	//left < right true
+	int A_size = A.size();
 
-	int g_size = A.size();
-	int s_size = A[0].size();
-	for (int size = 2; size <= m_size; size++) {
-		for (int i = 0; i < g_size; i++) {
-			for (int j = 0; j < s_size; j++) {
-				if (i + size - 1 >= g_size || j + size - 1 >= s_size)
-					continue;
-				res = res < window(size, i, j, A) ? window(size, i, j, A) : res;
-			}
+	if (A_size == 1)
+		return res;
+	bool toggle = check(0, 1, A);
+	for (int i = 1; i < A_size - 1; i++) {
+		bool t = check(i, i + 1, A);
+		res = res < right - left + 1 ? right - left + 1 : res;
+
+		if (A[i] == A[i + 1]) {
+			left = i + 1, right = i + 1;
+			i++;
+			if (i + 1 < A_size - 1)
+				toggle = check(A[i], A[i + 1], A);
+			continue;
 		}
 
+		if (t != toggle) {
+			right = i + 1;
+			toggle = t;
+			continue;
+		}
+		else if (t == toggle) {
+			right = i + 1;
+			left = i;
+		}
 	}
+
+	res = res < right - left + 1 ? right - left + 1 : res;
 	return res;
-}
-
-int main(void) {
-	ios::sync_with_stdio(false);
-
-	vector < vector<int> >A({ vector<int>(1) });
-
-
-
-	cout << solution(A);
 }

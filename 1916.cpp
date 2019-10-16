05f8f66de729fb1a73f12467fd46c240c6f02b2e
taxijjang <gw9122@Naver.com>
2019-10-16 17:02:26 +0900

    dijk

--- 1916.cpp
+++ 1916.cpp
@@ -7,52 +7,58 @@
 #define INF 2e9
 using namespace std;
 
-int dist[SIZE];
-vector<pair<int, int>> v[SIZE];
-void fill_city() {
-	for (int i = 0; i < SIZE; i++) {
-		dist[i] = INF;
+typedef struct Data {
+	int x, y, cost;
+}Data;
+
+struct cmp {
+	bool operator()(Data d1, Data d2) {
+		return d1.cost > d2.cost;
 	}
+};
+
+int dist[SIZE];
+vector<Data> dijk[SIZE];
+priority_queue<Data, vector<Data>, cmp> pq;
+
+void fill_dist() {
+	for (int i = 0; i < SIZE; i++)
+		dist[i] = INF;
 }
-
-priority_queue<pair<int,int>> pq;
-
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	fill_city();
+
+	fill_dist();
 	int N, M; cin >> N >> M;
 
-
 	for (int i = 0; i < M; i++) {
-		int dx, dy, dcost;
-		cin >> dx >> dy >> dcost;
-		v[dx].push_back({ dcost,dy });
+		Data here;
+		cin >> here.x >> here.y >> here.cost;
+		dijk[here.x].push_back(here);
 	}
 
 	int start, end; cin >> start >> end;
 
-	dist[start] = 0;
-	
-	pq.push({ 0,start });
+	Data s; s.x = start; s.y = end; s.cost = 0;
+	dist[s.x] = 0;
+	pq.push(s);
 
-	
 	while (!pq.empty()) {
-		int cost = -pq.top().first;
-		int here = pq.top().second;
-
+		Data here = pq.top();
 		pq.pop();
-		for (auto n : v[here]) {
-			int n_cost = n.first;
-			int next = n.second;
 
-			if (dist[here] + n_cost < dist[next]) {
-				dist[next] = dist[here] + n_cost;
-				pq.push({ -dist[next],next });
+		for (auto n : dijk[here.x]) {
+			Data next = n;
+			if (dist[here.x] + next.cost < dist[next.y]) {
+				dist[next.y] = dist[here.x] + next.cost;
+
+				next.cost = dist[next.y];
+				next.x = next.y;
+				pq.push(next);
 			}
 		}
 	}
 
 	cout << dist[end];
-
 }

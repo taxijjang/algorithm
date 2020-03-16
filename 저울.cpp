924565b916aea53aafa79572ccf2945e8d63f5fa
taxijjang <gw9122@naver.com>
2020-03-16 17:06:24 +0900

    저울

--- 저울.cpp
+++ 저울.cpp
@@ -17,30 +17,25 @@ using namespace std;
 
 typedef long long int ll;
 
-int prefix[SIZE];
-map<int, bool> _map;
 int solution(vector<int> weight) {
 	sort(weight.begin(), weight.end());
-	
+
 	if (weight[0] != 1) {
 		return 1;
 	}
 
-	prefix[0] = 1;
-	for (int i = 1; i < weight.size(); i++) {
-		prefix[i] = prefix[i - 1] + weight[i];
-
-		if (prefix[i - 1] < weight[i])
-			return prefix[i] + 1;
+	int ans = 1;
+	for (int i = 0; i < weight.size(); i++) {
+		if (ans >= weight[i]) ans += weight[i];
 	}
 
-	return prefix[weight.size()-1]+ 1;
+	return ans;
 }
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	vector<int> weights = { 1,2,3};
+	vector<int> weights = { 1,1, };
 
 	cout << solution(weights);
 }

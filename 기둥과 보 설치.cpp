9e7b8db72b32253576b15abed6fe8723daf5334f
taxijjang <gw9122@Naver.com>
2020-03-19 00:20:09 +0900

    기둥과 보 섳치(3일만에 품)

--- 기둥과 보 설치.cpp
+++ 기둥과 보 설치.cpp
@@ -17,132 +17,67 @@ using namespace std;
 
 typedef long long int ll;
 
-map<int, map<int, bool>> gi;
-map<int, map<int, bool>> bo;
 
+map<tuple<int, int, int>, int> ress;
 bool cmp(vector<int> v1, vector<int> v2) {
 	if (v1[0] == v2[0]) {
-		if (v1[1] == v2[0]) {
+		if (v1[1] == v2[1]) {
 			return v1[2] < v2[2];
 		}
-		return v1[1] < v2[2];
+		return v1[1] < v2[1];
 	}
 	return v1[0] < v2[0];
+}
+
+bool check() {
+	for (auto r : ress) {
+		int x, y, a; tie(x, y, a) = r.first;
+
+		if (a == 0) {//기둥
+			if (y == 0 || ress.count(make_tuple(x - 1, y, 1)) || ress.count(make_tuple(x, y, 1)) || ress.count(make_tuple(x, y - 1, 0)))
+				continue;
+			else
+				return false;
+		}
+
+		else if (a == 1) {//보
+			if (ress.count(make_tuple(x, y - 1, 0)) || ress.count(make_tuple(x + 1, y - 1, 0)) || (ress.count(make_tuple(x - 1, y, 1)) + ress.count(make_tuple(x + 1, y, 1)) == 2))
+				continue;
+			else return false;
+		}
+	}
+
+	return true;
 }
 vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
 	vector<vector<int>> answer;
 	for (auto build : build_frame) {
-		//a(0 기둥, 1 보)  , b(0 삭제, 1 설치)
-		int x = build[0], y = build[1], a = build[2], b = build[3];
-		if (b == 1) {//설치 할때
-			if (a == 0) {//기둥
-				if (y == 0 || gi[x][y - 1] == true || bo[x - 1][y] == true || bo[x][y] == true)
-					gi[x][y] = true;
+		tuple<int, int, int> b = make_tuple(build[0], build[1], build[2]);
+		int toggle = build[3];
+
+		if (toggle == 1) {//추가
+			ress[b] = true;
+
+			if (check() == false) {
+				ress.erase(b);
 			}
-			else if (a == 1) {//보
-				if (gi[x][y - 1] == true || gi[x + 1][y - 1] == true || (bo[x - 1][y] == true && bo[x + 1][y] == true)) {
-					bo[x][y] = true;
-				}
+		}
+		else if (toggle == 0) {//삭제
+			ress.erase(b);
+
+			if (check() == false) {
+				ress[b] = true;
 			}
 		}
 
-		else if (b == 0) {//삭제 할때
-			if (a == 0) {//기둥
-				if (gi[x][y + 1] == true)
-					continue;
-
-				int check = 0;
-				for (int i = x - 1; i >= -1; i--) {
-					if (bo[i][y + 1] != true) {
-						if (i == x - 1)
-							check++;
-						break;
-					}
-
-					if (gi[i][y] == true) {
-						check++;
-						break;
-					}
-
-				}
-				for (int i = x; i <= n; i++) {
-					if (bo[i][y + 1] != true) {
-						if (i == x)
-							check++;
-						break;
-					}
-
-					if (gi[i][y] == true) {
-						check++;
-						break;
-					}
-
-				}
-
-				if (check < 2)
-					continue;
-				gi[x][y] = false;
-
-			}
-
-			else if (a == 1) {//보
-				if ((gi[x][y] == true && bo[x - 1][y] != true) || (gi[x + 1][y] == true && bo[x + 1][y] != true))
-					continue;
-
-				int check = 0;
-				for (int i = x; i > x - 2; i--) {
-					if (bo[i][y] != true) {
-						if (i == x - 1)
-							check++;
-						break;
-					}
-					if (gi[i][y - 1] == true) {
-						check++;
-						break;
-					}
-				}
-
-				for (int i = x + 1; i <= x + 2; i++) {
-					if (bo[i][y] != true) {
-						if (i == x + 1)
-							check++;
-						break;
-					}
-					if (gi[i][y - 1] == true) {
-						check++;
-						break;
-					}
-				}
-
-				if (check < 2)
-					continue;
-
-				bo[x][y] = false;
-			}
-		}
-	}
-
-	for (auto g : gi) {
-		for (auto gg : g.second) {
-			if (gg.second == true) {
-				vector<int> a;
-				a.push_back(g.first); a.push_back(gg.first); a.push_back(0);
-				answer.push_back(a);
-			}
-		}
 	}
 
 
-	for (auto b : bo) {
-		for (auto bb : b.second) {
-			if (bb.second == true) {
-				vector<int> a;
-				a.push_back(b.first); a.push_back(bb.first); a.push_back(1);
-				answer.push_back(a);
-			}
-		}
+	for (auto res : ress) {
+		int x, y, a; tie(x, y, a) = res.first;
+		vector<int> r = { x,y,a };
+		answer.push_back(r);
 	}
-
 	sort(answer.begin(), answer.end(), cmp);
 
 	return answer;
@@ -153,8 +88,8 @@ int main(void) {
 	cin.tie(NULL); cout.tie(NULL);
 
 
-	vector<vector<int>> bb = { {1, 0, 0, 1}, {1, 1, 1, 1}, {2, 1, 0, 1}, {2, 2, 1, 1}, {5, 0, 0, 1}, {5, 1, 0, 1}, {4, 2, 1, 1}, {3, 2, 1, 1} };
-	//vector<vector<int>> bb = {{0, 0, 0, 1}, {2, 0, 0, 1}, {4, 0, 0, 1}, {0, 1, 1, 1}, {1, 1, 1, 1}, {2, 1, 1, 1}, {3, 1, 1, 1}, {2, 0, 0, 0}, {1, 1, 1, 0}, {2, 2, 0, 1}};
+	//vector<vector<int>> bb = { {1, 0, 0, 1}, {1, 1, 1, 1}, {2, 1, 0, 1}, {2, 2, 1, 1}, {5, 0, 0, 1}, {5, 1, 0, 1}, {4, 2, 1, 1}, {3, 2, 1, 1} };
+	vector<vector<int>> bb = { {0, 0, 0, 1}, {2, 0, 0, 1}, {4, 0, 0, 1}, {0, 1, 1, 1}, {1, 1, 1, 1}, {2, 1, 1, 1}, {3, 1, 1, 1}, {2, 0, 0, 0}, {1, 1, 1, 0}, {2, 2, 0, 1} };
 	//vector<vector<int>> bb = { {1,0,0,1},{2,0,0,1},{0,1,1,1},{1,1,1,1},{1,1,1,0} };
 
 	/*int n; cin >> n;
@@ -165,10 +100,10 @@ int main(void) {
 		bb.push_back(bid);
 	}*/
 
-	vector<vector<int>> res = solution(5, bb);
+	vector<vector<int>> rr = solution(5, bb);
 
-	cout << "\n" << res.size() << "\n";
-	for (auto r : res) {
+	cout << "\n" << rr.size() << "\n";
+	for (auto r : rr) {
 		cout << r[0] << " " << r[1] << " " << r[2] << "\n";
 	}
 }

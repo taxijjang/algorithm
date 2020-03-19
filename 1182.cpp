1c0852636e3b6c11632615000557b9447e4e1739
taxijjang <gw9122@naver.com>
2020-03-19 16:59:08 +0900

    bitmask

--- 1182.cpp
+++ 1182.cpp
@@ -1,35 +1,45 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<string>
+#include<set>
+#include<map>
+#include<cstring>
+#include<functional>
+#include<cmath>
+#include<stack>
+
+#define SIZE 1010000
+const int INF = 2000000000;
 
 using namespace std;
 
-int arr[21];
-int N, S, ans = 0;
+typedef long long int ll;
 
-void recursive(int cur, int sum) {
-	if (cur == N + 1) {
-		if (sum == S) {
-			ans++;
-		}
-		return;
-	}
-	recursive(cur + 1, sum);
-	recursive(cur + 1, sum + arr[cur]);
-}
+int arr[1 << 22];
+
 int main(void) {
-	scanf("%d %d", &N, &S);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-	for (int i = 1; i <= N; i++) {
-		scanf("%d", &arr[i]);
+	int N, S; cin >> N >> S;
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[(1 << i)];
 	}
 
-	recursive(0, 1);
-
-	if (S == 0) {
-		ans--;
+	int cnt = 0;
+	for (int i = 1; i <(1 << N); i++) {
+		int sum = 0;
+		for (int j = 0; (1 << j) <= i; j++) {
+			if (i & (1 << j)) {
+				sum += arr[(1 << j)];
+			}
+		}
+		if (sum == S) {
+			cnt++;
+		}
 	}
-
-	printf("%d", ans);
-
-	return 0;
+	cout << cnt;
 }
-

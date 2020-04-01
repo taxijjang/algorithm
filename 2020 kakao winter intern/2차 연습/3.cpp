c2b6216c7049ddfc9825d5690f648b3971a0b8c1
taxijjang <gw9122@naver.com>
2020-04-01 15:54:28 +0900

    kakao inten

--- 2020 kakao winter intern/2차 연습/3.cpp
+++ 2020 kakao winter intern/2차 연습/3.cpp
@@ -10,44 +10,44 @@ bool visited[SIZE];
 set<string> _set;
 
 void dfs(vector<string> user_id, vector<string> banned_id, int b_index) {
+	int u_size = user_id.size();
+	int b_size = banned_id.size();
 
-   if (b_index >= banned_id.size()) {
-      string res = "";
-      for (int i = 0; i < user_id.size(); i++) {
-         if (visited[i]) {
-            res += i;
-         }
-      }
-      _set.insert(res);
-      return;
-   }
+	if (b_index >= b_size) {
+		string res = "";
+		for (int i = 0; i < user_id.size(); i++) {
+			if (visited[i]) {
+				res += i;
+			}
+		}
+		_set.insert(res);
+		return;
+	}
 
-   int u_size = user_id.size();
-   int b_size = banned_id.size();
 
-   for (int i = 0; i < u_size; i++) {
-      if (banned_id[b_index].size() != user_id[i].size() || visited[i]) {
-         continue;
-      }
+	for (int i = 0; i < u_size; i++) {
+		if (banned_id[b_index].size() != user_id[i].size() || visited[i]) {
+			continue;
+		}
 
-      bool toggle = true;
+		bool toggle = true;
 
-      for (int j = 0; j < user_id[i].size(); j++) {
-         if (banned_id[b_index][j] == '*') {
-            continue;
-         }
-         if (user_id[i][j] != banned_id[b_index][j]) {
-            toggle = false;
-            break;
-         }
-      }
+		for (int j = 0; j < user_id[i].size(); j++) {
+			if (banned_id[b_index][j] == '*') {
+				continue;
+			}
+			if (user_id[i][j] != banned_id[b_index][j]) {
+				toggle = false;
+				break;
+			}
+		}
 
-      if (toggle) {
-         visited[i] = true;
-         dfs(user_id, banned_id, b_index + 1);
-         visited[i] = false;
-      }
-   }   
+		if (toggle) {
+			visited[i] = true;
+			dfs(user_id, banned_id, b_index + 1);
+			visited[i] = false;
+		}
+	}
 }
 int solution(vector<string> user_id, vector<string> banned_id) {
    int answer = 0;
--- 2020 kakao winter intern/2차 연습/4.cpp
+++ 2020 kakao winter intern/2차 연습/4.cpp
@@ -27,15 +27,7 @@ ll _find(ll a) {
 
    return parrent[a];
 }
-void _union(ll a, ll b) {
-   ll aRoot = _find(parrent[a]);
-   ll bRoot = _find(parrent[b]);
 
-   if (aRoot == bRoot)
-      return;
-
-   parrent[b] = aRoot;
-}
 vector<long long> solution(long long k, vector<long long> room_number) {
    vector<long long> answer;
 
@@ -43,17 +35,7 @@ vector<long long> solution(long long k, vector<long long> room_number) {
       ll next = _find(room);
       answer.push_back(next);
       parrent[next] = _find(next + 1);
-      /*if (parrent[room] == 0) {
-         answer.push_back(room);
-         ll next = _find(room+1);
-         parrent[room] = next;
-      }
-      else if (parrent[room] != 0) {
-         ll next = _find(room);
-         answer.push_back(next);
-         ll nnext = _find(next + 1);
-         parrent[next] = nnext;
-      }*/
+  
    }
    return answer;
 }

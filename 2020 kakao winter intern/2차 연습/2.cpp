05b20f6ca73831b75ca39eb9f27286affd73e9c4
taxijjang <gw9122@naver.com>
2020-04-02 16:55:47 +0900

    튜플

--- 2020 kakao winter intern/2차 연습/2.cpp
+++ 2020 kakao winter intern/2차 연습/2.cpp
@@ -1,74 +1,41 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
-#include<queue>
-#include<string>
-#include<set>
 #include<map>
-#include<cstring>
-#include<functional>
-#include<cmath>
-#include<stack>
-
-#define SIZE 100100
-const int INF = 2000000000;
+#include<algorithm>
+#include<string>
+#include<vector>
 
 using namespace std;
 
-typedef long long int ll;
+map<string, int> _map;
 
-map<int, map<string,int>> _map;
-map<string,bool> visited;
+bool cmp(pair<string, int> p1, pair<string, int>p2) {
+	return p1.second > p2.second;
+}
 vector<int> solution(string s) {
-   int comma_cnt = 0;
-   string num = "";
-   map<string, int> _m;
-   bool inner_comma= true;
-   int inner_cnt = 0;
-   for (int i = 1; i < s.size() - 1; i++) {
-      if (s.at(i) == '{') {
-         num = "";
-         comma_cnt = 0;
-         _m.clear();
-         inner_comma = true;
-         inner_cnt = 0;
-      }
-      else if (s.at(i) == '}') {
-         _m[num] = true;
-         _map[comma_cnt + 1] = _m;
-         inner_comma = false;
-      }
-      else if (s.at(i) == ',') {
-         if (inner_comma == true) {
-            _m[num] = true;
-            comma_cnt++;
-            num = "";
-            inner_cnt++;
-         }
+	string num = "";
+	for (int i = 1; i < s.size() - 1; i++) {
+		if (s[i] == '}' || s[i] == '{' || s[i] == ',') {
+			if (s[i] == ',' && num != "" || s[i] == '}' && num != "")
+				_map[num]++;
+			num = "";
+		}
+		else {
+			num = num + s[i];
+		}
+	}
 
-      }
-      else {//숫자 일때
-         num+= s.at(i);
-      }
-   }
-   vector<int> answer;
+	vector <pair<string, int>> arr(_map.begin(), _map.end());
+	sort(arr.begin(), arr.end(), cmp);
 
-   int m_size = _map.size();
-   for (int i = 1; i <= m_size; i++) {
-      for (auto _map : _map[i]) {
-         if (visited[_map.first] == false) {
-            answer.push_back(stoi(_map.first));
-            visited[_map.first] = true;
-         }
-      }
-   }
-   return answer;
+	vector<int> answer;
+	for (auto a : arr) {
+		answer.push_back(stoi(a.first));
+	}
+	return answer;
 }
 
 int main(void) {
-   ios::sync_with_stdio(false);
-   cin.tie(NULL); cout.tie(NULL);
+	string s; cin >> s;
 
-   string s = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
-   vector<int> res = solution(s);
+	vector<int> res = solution(s);
 }

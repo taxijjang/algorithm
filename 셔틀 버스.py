a962eb67a58726b68294d72e7731413aa6353c9d
taxijjang <gw9122@naver.com>
2020-10-17 18:19:01 +0900

    용규 도와주기

---
+++ 셔틀 버스.py
@@ -0,0 +1,18 @@
+import collections
+import heapq
+import functools
+import itertools
+import re
+import sys
+import math
+from typing import *
+
+def solution(n,t,m,timetable):
+    answer = 0
+
+    return answer
+
+if __name__=="__main__":
+    n,t, m = 1, 1, 5
+    timetable = ["08:00", "08:01", "08:02", "08:03"]
+    print(solution(n,t,m,timetable))

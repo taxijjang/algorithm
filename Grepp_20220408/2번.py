af1176ec44938edbe283d403202fb939007384cf
taxijjang <gw9122@naver.com>
2022-04-08 22:41:34 +0900

    Add comment Grepp coding test problem 4

--- Grepp_20220408/2번.py
+++ Grepp_20220408/2번.py
@@ -10,7 +10,7 @@ def get_index_count_alphabet(cards):
     return index_count_alphabet
 
 
-def total_count_alphabet(cards):
+def get_total_count_alphabet(cards):
     return collections.Counter(''.join(cards))
 
 
@@ -37,8 +37,14 @@ def do_where_alphabet_is_available(word, index_count):
 
 
 def solution(cards, words):
+    """
+    1. 먼저 카드에 있는 각각의 알파벳의 갯수를 구하고 단어로 만들 수 있는지 확인 했습니다.
+    2. [줄의 위치][알파벳]로 구분할 데이터를 만들어 놓은 후
+    3. 줄의 위치를 반복하여 [줄의 위치(0)]['A'], [줄의 위치(1)]['A'], [줄의 위치(2)]['A']이 존재하는지 확인 하여 set에 저장
+    4. data가 저장된 set의 길이를 확인 (카드의 길이는 3) 길이가 3이면 만들기 가능, 아니면 불가
+    """
     index_count = get_index_count_alphabet(cards=cards)
-    total_count = total_count_alphabet(cards=cards)
+    total_count = get_total_count_alphabet(cards=cards)
     answer = list()
     for word in words:
         if not do_alphabet_number_check(word=word, total_count=total_count):
@@ -46,12 +52,3 @@ def solution(cards, words):
         if do_where_alphabet_is_available(word=word, index_count=index_count):
             answer.append(word)
     return answer if answer else ["-1"]
-
-
-if __name__ == '__main__':
-    cards = ["ABACDEFG", "NOPQRSTU", "HIJKLKMM"]
-    words = ["GPQM", "GPMZ", "EFU", "MMNA"]
-
-    cards = ["AABBCCDD", "KKKKJJJJ", "MOMOMOMO"]
-    words = ["AAAKKKKKMMMMM", "ABCDKJ"]
-    print(solution(cards=cards, words=words))

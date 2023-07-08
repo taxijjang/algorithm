885121c3650e2745a89c6e6dec1cb2daf7defbd3
김택윤 <gw9122@naver.com>
2023-07-08 16:22:49 +0900

    토스 망..

--- openai/whisper_test.py
+++ openai/whisper_test.py
@@ -3,8 +3,24 @@ import whisper
 
 def main():
     model = whisper.load_model("base")
-    result = model.transcribe("news.mp4", language="ko", fp16=False)
-    print(result)
+
+    # load audio and pad/trim it to fit 30 seconds
+    audio = whisper.load_audio("audio.mp3")
+    audio = whisper.pad_or_trim(audio)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    print(f"Detected language: {max(probs, key=probs.get)}")
+
+    # decode the audio
+    options = whisper.DecodingOptions()
+    result = whisper.decode(model, mel, options)
+
+    # print the recognized text
+    print(result.text)
 
 
 if __name__ == "__main__":

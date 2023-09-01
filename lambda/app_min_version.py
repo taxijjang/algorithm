de68d88da6cafd340f3e77dd13cd704ef85dcd42
김택윤 <gw9122@naver.com>
2023-09-01 10:11:31 +0900

    asdf

--- lambda/app_min_version.py
+++ lambda/app_min_version.py
@@ -4,6 +4,6 @@ def main():
     url = "https://www.naver.com"
     response = requests.get(url)
     print(response.content)
-        response.headers.values()
+    response.headers.values()
 if __name__ == "__main__":
     main()
